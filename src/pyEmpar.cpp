9d685bbd783f1945cf2ace6b36eeea71a48cfa04
Anna Kedzierska <amk@Annas-MacBook-Air.local>
2016-06-29 17:03:29 -0400

    refactored Empar for bindings

--- src/pyEmpar.cpp
+++ src/pyEmpar.cpp
@@ -1,6 +1,11 @@
 #include <pybind11/pybind11.h>
 
 #include <Empar/src/Empar.h>
+#include <Empar/src/model.h>
+#include <Empar/src/parameters.h>
+#include <Empar/src/tree.h>
+#include <Empar/src/alignment.h>
+
 
 namespace py = pybind11;
 
@@ -9,5 +14,14 @@ PYBIND11_PLUGIN(empar) {
   py::module m("empar", "sample c++ bindings module");
 	m.def("run", &run, "main call");
 
+  py::class_<Model>(m, "Model");
+  py::class_<Parameters>(m, "Parameters");
+  py::class_<Tree>(m, "Tree");
+  py::class_<Counts>(m, "Counts");
+
+  m.def("create_model", &create_model, "Create the model");
+  m.def("read_tree", &read_tree, "Read the tree");
+  m.def("create_parameters", &create_parameters, "Create the parameters");
+  m.def("EMalgorithm", &EMalgorithm, "Run the EM algorithm");
 	return m.ptr();
 }
