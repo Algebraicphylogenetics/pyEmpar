90e9bcefe190b6bf55a03134fac063ffbe1f82ab
Anna Kedzierska <amk@Annas-MacBook-Air.local>
2016-07-23 21:35:21 +0200

    added functions modularized within the EM call

--- src/pyEmpar.cpp
+++ src/pyEmpar.cpp
@@ -5,6 +5,9 @@
 #include <Empar/src/parameters.h>
 #include <Empar/src/tree.h>
 #include <Empar/src/alignment.h>
+#include <Empar/src/em.h>
+#include <Empar/src/state_list.h>
+#include <Empar/src/matrix.h>
 
 
 namespace py = pybind11;
@@ -18,10 +21,18 @@ PYBIND11_PLUGIN(empar) {
   py::class_<Parameters>(m, "Parameters");
   py::class_<Tree>(m, "Tree");
   py::class_<Counts>(m, "Counts");
+  py::class_<Counts>(m, "Counts");
+  py::class_<StateList>(m, "StateList");
+  py::class_<Matrix>(m, "Matrix");
 
   m.def("create_model", &create_model, "Create the model");
   m.def("read_tree", &read_tree, "Read the tree");
   m.def("create_parameters", &create_parameters, "Create the parameters");
   m.def("EMalgorithm", &EMalgorithm, "Run the EM algorithm");
+  m.def("create_state_list", &create_state_list, "Create a list of states");
+
+  m.def("create_matrix", &create_matrix, "Create matrix");
+  m.def("log_likelihood_fast", &log_likelihood_fast, "Calculate loglik- fast version");
+  m.def("joint_prob_fast", &joint_prob_fast, "Calculate joint probability");
 	return m.ptr();
 }
